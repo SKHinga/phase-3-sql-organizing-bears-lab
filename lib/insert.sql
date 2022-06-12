a48371a3e2db7fa474245356cdbdec4af38e5084
Samuel Hinga <samuelhinga200@gmail.com>
2022-06-12 20:12:15 +0300

    commit tree 5c72c3b5c143e32847664de81865cf063d160928

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES 
+(1, 'Mr. Chocolate', 10, 'M', 'brown', 'ease', 1),
+(2, 'Rowdy', 8, 'M', 'white', 'difficult', 1),
+(3, 'Tabitha', 12, 'F', 'black', 'ease', 0),
+(4, 'Sergeant Brown', 15, 'M', 'brown', 'ease', 1),
+(5, 'Melissa', 7, 'F', 'black', 'ease', 1),
+(6, 'Grinch', 20, 'M', 'brown', 'ease', 0),
+(7, 'Wendy', 8, 'F', 'white', 'difficult', 1),
+(8, NULL, 10, 'F', 'brown', 'ease', 1);
